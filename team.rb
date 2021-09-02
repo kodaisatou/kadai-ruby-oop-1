d5932a1b8fed2fd95ec718309e2269fb18e03dd5
kota.s <koudai8314@gmail.com>
2021-09-02 22:05:08 +0000

    Lesson7課題

--- team.rb
+++ team.rb
@@ -1,21 +1,21 @@
 class Team
     
-    attr_accessor :name,:win,:lose,:draw
+  attr_accessor :name,:win,:lose,:draw
     
-    def initialize(name,win,lose,draw)
-        self.name = name
-        self.win = win
-        self.lose = lose
-        self.draw = draw
-    end
+  def initialize(name,win,lose,draw)
+    self.name = name
+    self.win = win
+    self.lose = lose
+    self.draw = draw
+  end
 
-    def calc_win_rate
-        self.win.to_f / (self.win + self.lose)
-    end
+  def calc_win_rate
+    self.win.to_f / (self.win + self.lose)
+  end
     
-    def show_team_result
-        p "#{self.name} の2020年の成績は#{self.win}勝 #{self.lose}敗 #{self.draw}分、勝率は#{self.calc_win_rate}です。"
-    end
+  def show_team_result
+    p "#{self.name} の2020年の成績は#{self.win}勝 #{self.lose}敗 #{self.draw}分、勝率は#{self.calc_win_rate}です。"
+  end
 end
 
 team1 = Team.new('Giants',67,45,8)
